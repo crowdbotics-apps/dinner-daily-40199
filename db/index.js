6dfb17a2bc1cbc23d64f514101548938f394738e
Prabhat_safi <prabhat_safi.nga.crowdbotics.com>
2023-06-16 11:14:29 +0530

    conflict

--- db/index.js
+++ db/index.js
@@ -6,18 +6,19 @@ const config = rfr('/shared/config');
 
 const dbObj = config['database'];
 
-// let pool = mysql.createPool(`mysql://${dbObj.username}:${dbObj.password}@${dbObj.host}/${dbObj.db}?reconnect=true`);
-console.log('inside database');
+//let pool = mysql.createPool(`mysql://${dbObj.username}:${dbObj.password}@${dbObj.host}/${dbObj.db}?reconnect=true`);
 
-// let pool = mysql.createPool('mysql://be2fbda272a899:df8486ac@us-cdbr-east-06.cleardb.net/heroku_52b3caaac7494c9?reconnect=true');
-let pool = mysql.createPool({
-  connectionLimit: 50,
-  host: 'us-cdbr-east-06.cleardb.net',
-  user: 'be2fbda272a899',
-  password: 'df8486ac',
-  database: 'heroku_52b3caaac7494c9',
-  maxIdle: 50
-})
+let pool = mysql.createPool(process.env.CLEARDB_AMBER_URL);
+
+let pool = mysql.createPool('mysql2://be2fbda272a899:df8486ac@us-cdbr-east-06.cleardb.net/heroku_52b3caaac7494c9?reconnect=true');
+// let pool = mysql.createPool({
+//   connectionLimit: 50,
+//   host: 'us-cdbr-east-06.cleardb.net',
+//   user: 'be2fbda272a899',
+//   password: 'df8486ac',
+//   database: 'heroku_52b3caaac7494c9',
+//   maxIdle: 50
+// })
 
 pool.getConnection((err, connection) => {
   if (err) {
